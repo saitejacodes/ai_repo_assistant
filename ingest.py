7a945f444b353275a9d1d1c17e91431cd9664112
saitejacodes <saitejaveeramalla3@gmail.com>
2026-03-22 20:44:15 +0530

    Optimize Jupyter notebook chunking parameters

--- ingest.py
+++ ingest.py
@@ -57,7 +57,7 @@ def chunk_documents(documents):
         separators=["\nclass ", "\ndef ", "\n\n", "\n", " "]
     )
     notebook_splitter = RecursiveCharacterTextSplitter(
-        chunk_size=500,
+        chunk_size=400,
         chunk_overlap=100,
         separators=["\n\n", "\n", " "]
     )
